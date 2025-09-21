519315d7bd9cf5505b9c1216b2d917fa14aa1ae7
Louis <louis.halley@utt.fr>
2025-09-22 00:06:42 +0200

    fix: url

--- src/scripts/url.js
+++ src/scripts/url.js
@@ -1,6 +1,6 @@
 function cleanURL() {
     const url = window.location.href;
-    const newURL = url.replace(/(index)?\.html$/, '');
+    const newURL = url.replace(/index\.html$/, '');
 
     if (newURL !== url) {
         window.history.replaceState({}, document.title, newURL);
