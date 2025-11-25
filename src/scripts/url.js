7d42a4d6fae9f7c2004daebf92b9de2383219fb4
AedanDeFrates <adefra5@lsu.edu>
2025-11-25 13:14:55 -0600

    [feat] commented url.js script

--- src/scripts/url.js
+++ src/scripts/url.js
@@ -1,7 +1,12 @@
+/**
+ * Cleans the URL by removing 'index.html' if present.
+ */
 function cleanURL() {
+    // Get the current URL
     const url = window.location.href;
+    // Remove 'index.html' from the URL if it exists
     const newURL = url.replace(/index\.html$/, '');
-
+    // Update the browser's URL without reloading the page
     if (newURL !== url) {
         window.history.replaceState({}, document.title, newURL);
     }
